660f5aa8b7ec70101977e60258c99323971a60c0
bilobom <bilobom124@gmail.com>
2024-08-28 08:11:29 +0000

    tests

--- test.test.js
+++ test.test.js
@@ -1,1 +1,13 @@
 
+test('test 1 of pr check before merge', async () => {
+    expect(true).toBeTruthy()
+})
+test('test 2 of pr check before merge', async () => {
+    expect(false).toBeFalsy()
+})
+test('test 3 of pr check before merge', async () => {
+    expect("false").toBe("false")
+})
+test('test 4 of pr check before merge', async () => {
+    expect("true").toBe("true")
+})
